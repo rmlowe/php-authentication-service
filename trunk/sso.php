a31eac1a74da756f2bd346631e24dfc59d2fb41b
robertmlowe <robertmlowe@2149ac40-9d37-e7be-e63e-1c60f0737a6d>
2011-04-21 00:36:54 +0000

    Digest calculation

--- trunk/sso.php
+++ trunk/sso.php
@@ -1,14 +1,22 @@
 <?php
+require 'config.php';
 session_start();
 
 $salt = $_REQUEST['salt'];
-$username = $_SESSION['username'];
+#$username = $_SESSION['username'];
+$username = 'ndadmin';
 
 if (isset($username)) {
-	echo 'It is set.';
+	$salt_bytes = base64_decode($salt);
+	$digest_base_string = $username . $key . $salt_bytes;
+	$digest_str = md5($digest_base_str, TRUE);
+	$digest = base64_encode($digest_str);
+	echo '<p>$salt_bytes: ' . $salt_bytes . '</p>';
+	echo '<p>$digest_base_string: ' . $digest_base_string . '</p>';
+	echo '<p>$digest_str: ' . $digest_str . '</p>';
+	echo '<p>$digest: ' . $digest . '</p>';
 } else {
 	$error = FALSE;
-#	echo $_SESSION['username'];
 	include 'signin.php';
 }
 ?>
